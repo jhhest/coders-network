218ce3bda5ca8749ade0d1a64aac6a23f3a6cd51
jhhest <janvanhest@outlook.com>
2020-01-31 23:38:54 +0100

    Added basic router functionality. Added routes to homepage and contactpage

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from "react";
+
+export class ContactPage extends Component {
+  render() {
+    return (
+      <Fragment>
+        <p>Here comes a contactpage</p>
+      </Fragment>
+    );
+  }
+}
+
+export default ContactPage;
